de90aa3e2b03be31fd9b0d4d45923d40829f1f14
RandyBao <locbaomedia23@gmail.com>
2026-05-29 23:23:16 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-
+base: '/quanlimatkhau/',
 export default defineConfig(() => {
   return {
     plugins: [
